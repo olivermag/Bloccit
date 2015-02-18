cfd0831bd069d984e2522df630030ff604e240db
Olivier Magny <olivier.magny1010@gmail.com>
2015-02-18 12:55:30 -0500

    Created post controller but cannot view comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
-  get "welcome/contact"
 
-   root to: 'welcome#index'
+	resources :posts
+
+	get 'about' => 'welcome#about'
+
+ root to: 'welcome#index'
 end
