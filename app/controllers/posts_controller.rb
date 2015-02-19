ce9088224b0517cb2b3b733c1b3265acd62678ae
Olivier Magny <olivier.magny1010@gmail.com>
2015-02-19 02:25:05 -0500

    Added CRUD interface

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
   end
@@ -8,8 +9,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Try again? :)"
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was gladly updated"
+      redirect_to @post
+    else
+      flash[:error] = "Hmm, we are unable to save the post... Sorry!"
+      render :edit
+    end
+
+end
 end
