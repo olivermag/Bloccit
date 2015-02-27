894ec7142d096690a4992d39998634d760002666
Olivier Magny <olivier.magny1010@gmail.com>
2015-02-26 21:32:32 -0500

    Authorization-checkpoint

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'faker'
 gem 'devise'
+gem 'pundit'
 gem 'figaro', '1.0'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
    before_action :configure_permitted_parameters, if: :devise_controller?
  
    protected
 
-     
+     rescue_from Pundit::NotAuthorizedError do |exception|
+      redirect_to root_url, alert: exception.message
+    end
+
    def configure_permitted_parameters
      devise_parameter_sanitizer.for(:sign_up) << :name
    end
