f49862db06e20f27e780079a726c9c2a03d01f6b
Olivier Magny <olivier.magny1010@gmail.com>
2015-04-09 17:24:06 -0400

    FEAT: Destroy action Added destroy action for comments, posts and topics with the appropriate permissions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,37 @@
-
 class CommentsController < ApplicationController
 
   def create
-
     @topic = Topic.find(params[:topic_id])
     @post  = Post.find(params[:post_id])
-
     @comment = Comment.new(comment_params)
-
     @comment.post = @post
     @comment.user = current_user
-
     if @comment.save
       flash[:notice] = "Comment was saved."
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      
-      
       flash[:errors_messages]=@comment.errors.messages
       redirect_to [@topic, @post]
     end
-
   end
+
+  def destroy
+    @topic= Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    authorize @comment
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@topic, @post]
+    end
+  end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:body)
